699902ba0762bb5c40fef7f577a5d89dc184dcba
Dominic Garms <djgarms@gmail.com>
2018-04-05 14:21:08 +0800

    merged changes

--- test/integration/article/nuxt.config.js
+++ test/integration/article/nuxt.config.js
@@ -7,10 +7,16 @@ module.exports = {
   render: {
     resourceHints: false
   },
+  // env: {
+  //   GRAPHQL_ALIAS: 'lumen-cms-demo',
+  //   GRAPH_FILE_API: 'cj8yj66xc01740164lh5bv4fz',
+  //   GRAPHQL_SUBSRIPTION: 'subscriptions.us-west-2.graph.cool'
+  // },
   env: {
-    GRAPHQL_ALIAS: 'lumen-cms-demo',
-    GRAPH_FILE_API: 'cj8yj66xc01740164lh5bv4fz',
-    GRAPHQL_SUBSRIPTION: 'subscriptions.us-west-2.graph.cool'
+    GRAPHQL_ALIAS: 'students-admin',
+    GRAPHQL_SUBSRIPTION: 'subscriptions.graph.cool',
+    GRAPH_FILE_API: 'ciwyoxmas3amt0129hec6zpvl',
+    COPYRIGHT: 'studentsgoabroad.com'
   },
   modules: [['@@', {
     pluginRoot: resolve(__dirname, '../../../lib/templates')
