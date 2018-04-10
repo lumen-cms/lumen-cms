1960557e0e5d792d38fc422629e6a2aa4794543e
Steffen Baer <stbaer.mail@gmail.com>
2018-04-10 13:32:22 +0800

    lint fixes

--- test/integration/article/nuxt.config.js
+++ test/integration/article/nuxt.config.js
@@ -1,39 +1,39 @@
 const {resolve} = require('path')
 
 module.exports = {
-    rootDir: resolve(__dirname, '../../..'),
-    srcDir: __dirname,
-    dev: false,
-    render: {
-        resourceHints: false
-    },
-    env: {
-        GRAPHQL_ALIAS: 'lumen-cms-demo',
-        GRAPH_FILE_API: 'cj8yj66xc01740164lh5bv4fz',
-        GRAPHQL_SUBSRIPTION: 'subscriptions.us-west-2.graph.cool'
-    },
-    // env: {
-    //   GRAPHQL_ALIAS: 'students-admin',
-    //   GRAPHQL_SUBSRIPTION: 'subscriptions.graph.cool',
-    //   GRAPH_FILE_API: 'ciwyoxmas3amt0129hec6zpvl',
-    //   COPYRIGHT: 'studentsgoabroad.com'
-    // },
-    'google-analytics': {
-        id: 'UA-22494234-1',
-        set: [
-            {field: 'anonymizeIp', value: true},
-            {field: 'forceSSL', value: true}
-        ]
-    },
+  rootDir: resolve(__dirname, '../../..'),
+  srcDir: __dirname,
+  dev: false,
+  render: {
+    resourceHints: false
+  },
+  env: {
+    GRAPHQL_ALIAS: 'lumen-cms-demo',
+    GRAPH_FILE_API: 'cj8yj66xc01740164lh5bv4fz',
+    GRAPHQL_SUBSRIPTION: 'subscriptions.us-west-2.graph.cool'
+  },
+  // env: {
+  //   GRAPHQL_ALIAS: 'students-admin',
+  //   GRAPHQL_SUBSRIPTION: 'subscriptions.graph.cool',
+  //   GRAPH_FILE_API: 'ciwyoxmas3amt0129hec6zpvl',
+  //   COPYRIGHT: 'studentsgoabroad.com'
+  // },
+  'google-analytics': {
+    id: 'UA-22494234-1',
+    set: [
+      {field: 'anonymizeIp', value: true},
+      {field: 'forceSSL', value: true}
+    ]
+  },
 
-    modules: [['@@', {
-        cms: {
-            pageToolbarExtension: false
-        }
-    }]],
-    build: {
-        extend (config) {
-            // config.resolve.alias['~articleUpdate'] = resolve(__dirname, './gql/UpdateArticle.gql')
-        }
+  modules: [['@@', {
+    cms: {
+      pageToolbarExtension: false
     }
+  }]],
+  build: {
+    extend (config) {
+      // config.resolve.alias['~articleUpdate'] = resolve(__dirname, './gql/UpdateArticle.gql')
+    }
+  }
 }
