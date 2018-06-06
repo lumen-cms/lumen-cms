728ba4563a0d17352305408baa75f9cf3a9d9cfd
Dominic Garms <djgarms@gmail.com>
2018-06-06 13:53:05 +0800

    make sure jumbotron has file object

--- lib/templates/components/view/partials/jumbotron.vue
+++ lib/templates/components/view/partials/jumbotron.vue
@@ -16,7 +16,7 @@
   </v-jumbotron>
 </template>
 <script>
-  import { getImageSrc } from '../../../util/imageSrcHelper'
+  import {getImageSrc} from '../../../util/imageSrcHelper'
   import getJumbotronCropValue from '../../../util/getJumbotronCropValue'
   import getViewportDimensions from '../../../util/getViewportDimensions'
 
@@ -77,11 +77,13 @@
       getSrc () {
         const ref = Object.assign({}, this.fileReference)
         ref.resize = ref.resize || 'x' + this.height // not sure if we should do any resizing actually
+        const file = this.fileReference.file
+        if (!file) {
+          return ''
+        }
+        const {xCropAmount, yCropAmount} = getJumbotronCropValue(this.height, file.height, file.width)
 
-        const {xCropAmount, yCropAmount} =
-          getJumbotronCropValue(this.height, this.fileReference.file.height, this.fileReference.file.width)
-
-        const {src} = getImageSrc(this.fileReference.file, null, `${xCropAmount}x${yCropAmount}centro`)
+        const {src} = getImageSrc(file, null, `${xCropAmount}x${yCropAmount}centro`)
         return src
       }
     }
