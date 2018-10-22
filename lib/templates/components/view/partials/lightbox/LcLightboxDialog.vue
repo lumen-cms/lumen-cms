b2fc8b0ea763186eb9a944d8e1ebd60947d4a1ac
Steffen Baer <stbaer.mail@gmail.com>
2018-10-22 09:41:26 +0800

    fix lightbox

--- lib/templates/components/view/partials/lightbox/LcLightboxDialog.vue
+++ lib/templates/components/view/partials/lightbox/LcLightboxDialog.vue
@@ -1,43 +1,45 @@
 <template>
-  <no-ssr>
-    <v-dialog content-class="lightbox-dialog"
-              max-width="100vw"
-              lazy
-              v-model="isActive"
-              @keydown.esc="isActive = false"
-              @keydown.right="carouselActiveItem++"
-              @keydown.left="carouselActiveItem--">
-      <div class="text-xs-right">
-        <v-spacer/>
-        <v-btn icon
-               flat
-               round
-               dark
-               @click="isActive=false">
-          <v-icon>close</v-icon>
-        </v-btn>
-      </div>
-      <v-card>
-        <v-card-text>
-          <lc-image-lightbox-item v-if="fileReferences.length === 1"
-                                  :content="fileReferences[0]"/>
-          <!--<lc-image-lightbox-item v-if="fileReferences.length === 1"-->
-          <!--:content="{fileRef:fileReferences[0],isLightbox:true}"/>-->
-          <v-carousel hide-delimiters
-                      dark
-                      :value="carouselActiveItem"
-                      :cycle="false"
-                      v-else>
-            <v-carousel-item v-for="(item,i) in fileReferences"
-                             :key="i"
-                             contain>
-              <lc-image-lightbox-item :content="item"/>
-            </v-carousel-item>
-          </v-carousel>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
-  </no-ssr>
+  <div>
+    <no-ssr>
+      <v-dialog content-class="lightbox-dialog"
+                max-width="100vw"
+                lazy
+                v-model="isActive"
+                @keydown.esc="isActive = false"
+                @keydown.right="carouselActiveItem++"
+                @keydown.left="carouselActiveItem--">
+        <div class="text-xs-right">
+          <v-spacer />
+          <v-btn icon
+                 flat
+                 round
+                 dark
+                 @click="isActive=false">
+            <v-icon>close</v-icon>
+          </v-btn>
+        </div>
+        <v-card>
+          <v-card-text>
+            <lc-image-lightbox-item v-if="fileReferences.length === 1"
+                                    :content="fileReferences[0]" />
+            <!--<lc-image-lightbox-item v-if="fileReferences.length === 1"-->
+            <!--:content="{fileRef:fileReferences[0],isLightbox:true}"/>-->
+            <v-carousel hide-delimiters
+                        dark
+                        :value="carouselActiveItem"
+                        :cycle="false"
+                        v-else>
+              <v-carousel-item v-for="(item,i) in fileReferences"
+                               :key="i"
+                               contain>
+                <lc-image-lightbox-item :content="item" />
+              </v-carousel-item>
+            </v-carousel>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+    </no-ssr>
+  </div>
 </template>
 <script>
   import LcImageLightboxItem from './LcImageLightboxItem'
@@ -48,7 +50,7 @@
   export default {
     name: 'LcLightboxDialog',
     mixins: [imageSourceSetMixin, imageSrcHelperMixin],
-    components: {LcImageLightboxItem},
+    components: { LcImageLightboxItem },
     props: {
       content: Object,
       fileReferences: Array
@@ -63,7 +65,7 @@
       lightboxItems () {
         return this.fileReferences.map(ref => {
           const img = this.getImageSrc(ref.file)
-          const {srcset, sizes} = this.getImageSourceSet(ref)
+          const { srcset, sizes } = this.getImageSourceSet(ref)
           return {
             src: img.src,
             srcset,
