0063c4d6657f5de26242c8ab78d97d702984a176
Steffen Baer <stbaer.mail@gmail.com>
2018-11-09 13:50:40 +0800

    fix for language handling

--- lib/templates/pages/index.vue
+++ lib/templates/pages/index.vue
@@ -97,8 +97,6 @@
         const data = await app.$axios.$get(url, config)
         const article = data.Article
         const urlAlias = data.UrlAlias
-        const articleLang = article && article.languageKey.toLowerCase()
-        console.log(articleLang, 'async article lang', slug, config.params, 'canEdit: ', store.getters.canEdit)
         if (article) {
           if (!store.getters.canEdit && (article.deleted || !article.published)) {
             error({
