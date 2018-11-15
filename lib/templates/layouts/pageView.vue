1b372c9b27d6d278f7f88c06916f5e52d928fa67
Steffen Baer <stbaer.mail@gmail.com>
2018-11-15 11:10:32 +0800

    test - use no-ssr for edit components in layout

--- lib/templates/layouts/pageView.vue
+++ lib/templates/layouts/pageView.vue
@@ -1,32 +1,34 @@
 <template>
   <v-app :class="{'jumbo-first': $store.state.lc.hasJumbotron && !$store.state.lc.isContentEditMode}">
-    <lc-sidebar-left/>
-    <lc-sidebar-right/>
-    <lc-page-toolbar/>
+    <lc-sidebar-left />
+    <lc-sidebar-right />
+    <lc-page-toolbar />
     <v-content class="page-view-layout"
                :class="{'is-loading':$store.state.lc.cmsLoading, 'content-edit-mode': $store.state.lc.isContentEditMode}">
       <v-container fluid
                    class="pa-0">
         <v-slide-y-transition mode="out-in">
-          <nuxt/>
+          <nuxt />
         </v-slide-y-transition>
       </v-container>
     </v-content>
-    <lc-main-footer/>
-    <lc-error-widget/>
-    <lc-admin-bar v-if="$store.getters.canEdit"
-                  :edit-route="{name: 'articleEdit', params: {id: $store.state.lc.pageProps.articleId}}"
-                  :add-route="{name:'articleEdit'}"
-                  :content-edit-toggle="true"/>
-    <template v-if="$store.state.lc.isContentEditMode">
-      <lc-media-library/>
-      <template v-if="!!$store.getters.getDialogType">
-        <lc-content-create ref="contentCreate"/>
-        <lc-content-delete-dialog :page-props="$store.state.lc.pageProps"
-                                  v-if="$store.getters.getDialogType === 'delete'"/>
-        <lc-content-edit-dialog v-if="$store.getters.getDialogType === 'edit'"/>
+    <lc-main-footer />
+    <lc-error-widget />
+    <no-ssr>
+      <lc-admin-bar v-if="$store.getters.canEdit"
+                    :edit-route="{name: 'articleEdit', params: {id: $store.state.lc.pageProps.articleId}}"
+                    :add-route="{name:'articleEdit'}"
+                    :content-edit-toggle="true" />
+      <template v-if="$store.state.lc.isContentEditMode">
+        <lc-media-library />
+        <template v-if="!!$store.getters.getDialogType">
+          <lc-content-create ref="contentCreate" />
+          <lc-content-delete-dialog :page-props="$store.state.lc.pageProps"
+                                    v-if="$store.getters.getDialogType === 'delete'" />
+          <lc-content-edit-dialog v-if="$store.getters.getDialogType === 'edit'" />
+        </template>
       </template>
-    </template>
+    </no-ssr>
   </v-app>
 </template>
 <script>
